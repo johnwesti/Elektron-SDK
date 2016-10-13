e56470f79085824bd4abb3e7ce402dfca81d4737
Tony Louras <tony.louras@thomsonreuters.com>
2016-10-13 14:38:17 -0500

    ETA-2186 first phase of open source effort for Java

---
+++ Java/Eta/Docs/WebDocs/TransportAPI_Java_Documentation_Portal/wwhdata/common/files.js
@@ -0,0 +1,31 @@
+function  WWHBookData_Files(P)
+{
+P.fA("Chapter 1 Getting Started","ETADocPortal.1.01.html");
+P.fA("1.1 Welcome","ETADocPortal.1.02.html");
+P.fA("1.2 Learning the Transport API for Java","ETADocPortal.1.03.html");
+P.fA("1.2.2 What is the Transport API Java Edition?","ETADocPortal.1.04.html");
+P.fA("1.2.3 New to OMM?","ETADocPortal.1.05.html");
+P.fA("1.2.4 Examples","ETADocPortal.1.06.html");
+P.fA("1.3 Learning Transport API Java Edition Value Added Components","ETADocPortal.1.07.html");
+P.fA("1.3.2 Developer\u2019s Guide","ETADocPortal.1.08.html");
+P.fA("1.3.3 Examples","ETADocPortal.1.09.html");
+P.fA("1.4 Getting Support","ETADocPortal.1.10.html");
+P.fA("1.4.2 SSG","ETADocPortal.1.11.html");
+P.fA("1.5 PDFs and Reference Manuals","ETADocPortal.1.12.html");
+P.fA("Chapter 2 Product Overview","ETADocPortal.1.13.html");
+P.fA("2.1 Overview","ETADocPortal.1.14.html");
+P.fA("2.2 Product Features","ETADocPortal.1.15.html");
+P.fA("2.2.2 Application-Specific Functionality","ETADocPortal.1.16.html");
+P.fA("2.2.3 Transport API Value Added Components Functionality","ETADocPortal.1.17.html");
+P.fA("2.3 Value Added Components Product Features","ETADocPortal.1.18.html");
+P.fA("Chapter 3 Performance Tool Suite","ETADocPortal.1.19.html");
+P.fA("3.1 About the Transport API Performance Tool Suite","ETADocPortal.1.20.html");
+P.fA("3.2 Transport API Performance Tools","ETADocPortal.1.21.html");
+P.fA("Chapter 4 Transport API Example Applications","ETADocPortal.1.22.html");
+P.fA("4.1 About Transport API Example Applications","ETADocPortal.1.23.html");
+P.fA("4.2 Running Examples","ETADocPortal.1.24.html");
+P.fA("4.3 Consumer and Provider Examples","ETADocPortal.1.25.html");
+P.fA("4.4 Value Added Component Examples","ETADocPortal.1.26.html");
+P.fA("4.5 Performance Tools","ETADocPortal.1.27.html");
+P.fA("4.6 DACS Lock Example","ETADocPortal.1.28.html");
+}
---
+++ Java/Eta/README.md
@@ -0,0 +1,19 @@
+# ETA Java Open Sourcing Effort
+
+Elektron Transport API Java Edition is currently undergoing work to fully open source all of its source code.
+This is a work in progress, but we wanted to share it on our GitHub preview branch.  
+
+## Contents: 
+The ETA Preview branch contains code from the master branch and has also added
+- 'Source/interface' which contains java files associated with the ETA interfaces
+- 'Source/impl' which contains the java implementation files for non-JNI based functionality of ETA
+
+The additional code provided is for preview purposes only and it is highly likely to continue to change and be restructured as we progress with the open sourcing effort.  
+
+
+## Notes:
+
+- Please make sure to review the LICENSE.md file.
+
+
+Check back over the coming months to see where we are at!
